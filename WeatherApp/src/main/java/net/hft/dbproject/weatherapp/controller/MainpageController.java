6bd849998d1722537678a4ea70414898f48ffa94
janhorak <janhorak@gmx.de>
2015-04-14 20:26:32 +0200

    W12: InternetHeartbeat implemented. Logger configured

--- WeatherApp/src/main/java/net/hft/dbproject/weatherapp/controller/MainpageController.java
+++ WeatherApp/src/main/java/net/hft/dbproject/weatherapp/controller/MainpageController.java
@@ -6,9 +6,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import net.hft.dbproject.weatherapp.services.InetHeartBeat;
 import net.hft.dbproject.weatherapp.uiactions.Mainpageactions;
+import org.apache.log4j.Logger;
 
 public class MainpageController implements Initializable {
+    
+    private Logger logger;
 
     @FXML
     private Pane mainpagePane;
@@ -18,14 +22,23 @@ public class MainpageController implements Initializable {
 
     @FXML
     private ImageView moveImage;
+    
+    @FXML
+    private ImageView inetConImage;
+    
+    private InetHeartBeat heartBeat;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        this.logger = Logger.getLogger(MainpageController.class);
+        this.heartBeat = new InetHeartBeat(inetConImage);
+        this.heartBeat.start();
         initUIActions();
+        this.logger.info("Started completely");
     }
 
     private void initUIActions() {
-
+        this.logger.info("Loading UI- Actions...");
         // Loading actions
         Mainpageactions actions = new Mainpageactions();
         
@@ -38,5 +51,6 @@ public class MainpageController implements Initializable {
         
         // Setting Exit- Action to exit- Icon
         exitImage.setOnMouseClicked(actions.exitEvent);
+        this.logger.info("Loading UI- Actions... done");
     }
 }
---
+++ WeatherApp/src/main/java/net/hft/dbproject/weatherapp/services/InetChecker.java
@@ -0,0 +1,37 @@
+package net.hft.dbproject.weatherapp.services;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ *
+ * @author Jan
+ */
+public abstract class InetChecker {
+
+    public static boolean isInternetReachable() {
+
+        URL url = null;
+        HttpURLConnection urlConnect = null;
+        int resultCode = 404;
+        
+        try {
+            url = new URL("http://www.google.com");
+            urlConnect = (HttpURLConnection) url.openConnection();
+            resultCode = urlConnect.getResponseCode();
+        } catch (MalformedURLException ex) {
+            return false;
+        } catch (IOException ex) {
+            return false;
+        }
+        if (resultCode == 200) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+}
