187c4278cdadd47e769638e8542f30a70345c14b
AllaShhapira <32shal1ms@hft-stuttgart.de>
2015-04-26 14:19:50 +0200

    Weather Service + Json parser

--- WeatherApp/src/main/java/net/hft/dbproject/weatherapp/entities/Weather.java
+++ WeatherApp/src/main/java/net/hft/dbproject/weatherapp/entities/Weather.java
@@ -6,6 +6,7 @@
 package net.hft.dbproject.weatherapp.entities;
 
 import java.io.Serializable;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -14,6 +15,7 @@ import javax.persistence.MappedSuperclass;
  *
  * @author admin
  */
+@Entity
 public class Weather implements Serializable{
   
     private String cityName;
--- WeatherApp/src/main/java/net/hft/dbproject/weatherapp/main/MainApp.java
+++ WeatherApp/src/main/java/net/hft/dbproject/weatherapp/main/MainApp.java
@@ -6,6 +6,7 @@ import javafx.stage.Stage;
 import net.hft.dbproject.weatherapp.enums.CSSFile;
 import net.hft.dbproject.weatherapp.manager.StageFunctionalities;
 import net.hft.dbproject.weatherapp.manager.Stagemanager;
+import net.hft.dbproject.weatherapp.services.WeatherService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,18 +23,26 @@ import com.google.gson.Gson;
 import com.google.gson.stream.JsonReader;
 import net.hft.dbproject.weatherapp.entities.Weather;
 
+import java.io.StringReader;
+import java.util.logging.Level;
+import net.hft.dbproject.weatherapp.services.WeatherService;
 public class MainApp extends Application {
 
     private StageFunctionalities manager;
     private static final Logger LOGGER = LoggerFactory.getLogger(MainApp.class);
-    
     private static final String APIKEY = "8207b192ff2c645813be5259681c74d6";
+    
+
 
     @Override
     public void start(Stage stage) throws Exception {
         this.manager = new Stagemanager();
         manager.openStageAsRoot(stage, getClass().getResource("/fxml/mainpage/Scene.fxml"), CSSFile.CSS_DEFAULT, 251, 397, false);
         
+       
+	  
+                
+                
         LOGGER.info("MainApp started successfully");
     }
 
@@ -48,57 +57,16 @@ public class MainApp extends Application {
     
     public static void main(String[] args) {
         try {
-            URL url = null;
-            HttpURLConnection urlConnect = null;
-            url = new URL("http://api.openweathermap.org/data/2.5/weather?q=London,uk");
-            urlConnect = (HttpURLConnection) url.openConnection();
-            urlConnect.setRequestMethod("GET");
-            urlConnect.setRequestProperty("Authorization", "APPID=" + APIKEY);
-            //urlConnect.addRequestProperty("x-api-key", APIKEY);
-            //urlConnect.setRequestProperty("APPID=", APIKEY);
-            urlConnect.setRequestProperty("Accept", "application/json");
-            InputStreamReader inputStream = new InputStreamReader(urlConnect.getInputStream());
-            BufferedReader br = new BufferedReader(inputStream);
-            String output;
-            System.out.println("Output from Server .... \n");
-		while ((output = br.readLine()) != null) {
-                    System.out.println(output);
-		}
-                
-            Weather myWeather = readWeather(inputStream);
-            System.out.println("Output from Class .... \n");
-                    System.out.println(myWeather.toString());
-		urlConnect.disconnect();
-	  } catch (MalformedURLException e) {
- 
-		e.printStackTrace();
- 
-	  } catch (IOException e) {
- 
-		e.printStackTrace();
-	  }
-        
-        launch(args);
-	}
-    
-        public static Weather readWeather(InputStreamReader streamReader) throws IOException
-        {
-            JsonReader reader = new JsonReader (streamReader);
-            String cname = "";
-            reader.beginObject();
-            while(reader.hasNext())
-            {
-                String name = reader.nextName();
-                if (name.equals("name"))
-                {
-                    cname = reader.nextString();
-                }     
-            }
-            reader.endObject();
-            reader.close();
-            return new Weather(cname);
+            URL url = new URL("http://api.openweathermap.org/data/2.5/weather?q=Stuttgart,d");
+            WeatherService service = new WeatherService(url, APIKEY);
+        } catch (MalformedURLException ex) {
+            java.util.logging.Logger.getLogger(WeatherService.class.getName()).log(Level.SEVERE, null, ex);
         }
+        launch(args);
     }
+    
+    }
+     
     
     
 
